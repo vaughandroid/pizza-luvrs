73eade8f215b83868d4ac4c72a3fa5b06194ff0d
Chris Vaughan <vaughandroid@gmail.com>
2019-08-26 07:14:16 +0100

    Add RDS database integration.

---
+++ data/pizzaStore.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+const database = 'pizza_luvrs',
+  host = 'pizza-db.cxmvgsiqymai.eu-west-1.rds.amazonaws.com',
+  username = 'postgres',
+  password = '54K!f^u5GoVr';
+
+const pgClient = new Sequelize(database, username, password, {
+    host: host,
+    dialect: 'postgres'
+});
+
+const Pizza = pgClient.define('pizza', {
+    id: { type: Sequelize.STRING, primaryKey: true },
+    name: { type: Sequelize.STRING },
+    toppings: { type: Sequelize.STRING },
+    image: { type: Sequelize.STRING },
+    username: { type: Sequelize.STRING },
+    created: { type: Sequelize.BIGINT }
+});
+
+Pizza.sync().then(() => {
+    console.log('Postgres connection ready.');
+});
+
+module.exports = Pizza;
--- data/pizzas.js
+++ data/pizzas.js
@@ -3,39 +3,64 @@
 const _ = require('lodash'),
   Pizza = require('../models/pizza'),
   ImageStore = require('../lib/imageStore'),
-  pizzas = {};
+  PizzaStore = require('./pizzaStore');
 
 function createPizza (name, toppings, img, username, callback) {
   ImageStore.saveImage(name.replace(/ /g, '-'), img, (err, imgUrl) => {
     if (err) throw err;
 
     let pizza = new Pizza(name, toppings, imgUrl, username);
-    pizzas[pizza.id] = pizza;
-    callback(null, pizza);
+    PizzaStore.create(prepPizza(pizza)).then(() => {
+      callback(null, pizza);
+    });
   });
 }
 
 // for mocks that don't need pizza images saved
 function importPizza (name, toppings, imgUrl, username) {
   let pizza = new Pizza(name, toppings, imgUrl, username);
-  pizzas[pizza.id] = pizza;
+  PizzaStore.create(prepPizza(pizza));
 }
 
 function getPizzaForUser (username, callback) {
-  let userPizzas = _.filter(pizzas, (pizza) => {
-    return pizza.username === username;
+  PizzaStore.findAll({
+    where: { username: username }
+  }).then((pizzas) => {
+    callback(null, debriefPizzas(pizzas));
   });
-  callback(null, userPizzas);
 }
 
 function getRecentPizzas (callback) {
-  let recentPizzas = _.orderBy(pizzas, ['created'], ['desc']);
-  callback(null, _.values(recentPizzas).splice(0, 5));
+  PizzaStore.findAll({
+    order: [['created', 'DESC']],
+    limit: 4
+  }).then((pizzas) => {
+    callback(null, debriefPizzas(pizzas));
+  });
 }
 
 function getPizza (pizzaId, callback) {
-  if (!pizzas[pizzaId]) callback('Pizza not found');
-  else callback(null, pizzas[pizzaId]);
+  PizzaStore.find({
+    where: { id: pizzaId }
+  }).then((pizza) => {
+    if (!pizza) {
+      callback('Could not find pizza.');
+    } else {
+      callback(null, debriefPizza(pizza));
+    }
+  });
+}
+
+function prepPizza (pizza) {
+  return _.assign(pizza, { toppings: JSON.stringify(pizza.toppings) });
+}
+
+function debriefPizza (pizza) {
+  return _.assign(pizza, { toppings: JSON.parse(pizza.toppings) });
+}
+
+function debriefPizzas (pizzas) {
+  return pizzas.map((pizza) => { return debriefPizza(pizza); });
 }
 
 module.exports.createPizza = createPizza;
